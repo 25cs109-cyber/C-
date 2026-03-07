496fffb1edb6243cfd12908ebbfb0dde042e2be3
Misari Vadalia <25cs109@charusat.edu.in>
2026-03-07 17:39:11 +0530

    Add files via upload

---
+++ exp.2.1.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle {
+    float length[10], width[10];
+    float perimeter[10], area[10];
+    int n;
+
+public:
+    Rectangle() {
+        cout << "Enter number of rectangles (<10): ";
+        cin >> n;
+
+        for(int i = 0; i < n; i++) {
+            do {
+                cout << "Enter length for rectangle " << i << ": ";
+                cin >> length[i];
+            } while(length[i] < 0);
+
+            do {
+                cout << "Enter width for rectangle " << i << ": ";
+                cin >> width[i];
+            } while(width[i] < 0);
+        }
+    }
+
+    void recPerimeter() {
+        for(int i = 0; i < n; i++) {
+            perimeter[i] = 2 * (length[i] + width[i]);
+        }
+    }
+
+    void recArea() {
+        for(int i = 0; i < n; i++) {
+            area[i] = length[i] * width[i];
+        }
+    }
+
+    void updateRec() {
+        char ch;
+        cout << "Any update? (Y/N): ";
+        cin >> ch;
+
+        if(ch == 'Y') {
+            int index;
+            cout << "Enter rectangle number: ";
+            cin >> index;
+
+            char op;
+            cout << "Update Length or Width (L/W): ";
+            cin >> op;
+
+            if(op == 'L') {
+                cout << "Enter new length: ";
+                cin >> length[index];
+            }
+            else {
+                cout << "Enter new width: ";
+                cin >> width[index];
+            }
+        }
+    }
+
+    void display() {
+        for(int i = 0; i < n; i++) {
+            cout << "\nRectangle " << i << endl;
+            cout << "Length = " << length[i] << endl;
+            cout << "Width = " << width[i] << endl;
+            cout << "Area = " << area[i] << endl;
+            cout << "Perimeter = " << perimeter[i] << endl;
+        }
+    }
+};
+
+int main() {
+    Rectangle r;
+    r.recPerimeter();
+    r.recArea();
+    r.updateRec();
+    r.display();
+}
